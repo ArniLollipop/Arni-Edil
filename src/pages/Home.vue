e3a17e056f2f70811578ce56387ee0cc2ab3dfe5
ArniLollipop <bocunahero@gmail.com>
2022-10-28 02:44:25 +0600

    qwe

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,10 +3,10 @@
     <img src="@/img/Баннер.png" alt="" class = "w-full">
     <div class="mt-16 grid grid-cols-9 gap-y-3 gap-x-14 md:hidden xl:grid-cols-6 lg:gap-x-2 xl:gap-y-5">
       <div class = "mx-auto text-center text-base w-[130px]" v-for="rec in recomendation" >
-        <div v-for="source in list_of_images" :rec="source">
-          <img :src="source.image" alt="">
+        <div class = "py-8 px-8 bg-white rounded-full flex items-center justify-center">
+          <img :src="rec.image" alt="" class = "">
         </div>
-        <a href = "#" class = "hover:text-accent">{{ rec }}</a>
+        <a href = "#" class = "hover:text-accent">{{ rec.title }}</a>
       </div>
     </div>
   </section>
@@ -16,10 +16,30 @@
   export default{
     data(){
       return {
-        list_of_images : [
-          {id:1, image : '@/img/Medved.jpg'}
+        recomendation:[
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
+          {title: 'Дитячі товари', image: '@/img/Medved.svg'},
         ],
-        recomendation:['Дитячі товари', 'Нерухомість', 'Для дому та комерції', 'Робота', 'Послуги та фріланс', 'Авто', 'Запчастини для транспорту', 'Тварини', 'Електроніка', 'Допомога', 'Спорт і відпочинок', 'Обладнання та сировина', 'Будматеріали та інструменти', 'Мода та стиль', 'Краса і здоров’я', 'Обмін', 'Для бізнесу', 'Хобі та розваги']
+        images:[
+          '@/img/Medved.jpg',
+          '@/img/Medved.svg'
+        ]
       }
     }
   }
