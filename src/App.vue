707ce1a5564dfa9880064ecdbb39c3b6301ad535
Kulzhabay Edil <edil.kulzhabay01@gmail.com>
2022-10-28 20:16:43 +0600

    Images require

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <template>
-  <div id = "App">
-    <main>
-      <router-view/>
+  <div id="App">
+    <main class="container mx-auto px-5">
+      <router-view />
     </main>
   </div>
 </template>
 
 <script>
-  
-  export default {
-    name : 'app',
-    data(){
-      return {
-        recomendation:[
-          {id: 1, title: 'Дитячі товари', image: 'img/Medved.png'},
-          
-        ],
-        images:[
-          '@/img/Medved.jpg',
-          '@/img/Medved.svg'
-        ]
-      }
-    }
-  }
-  
+export default {
+  name: "app",
+  data() {
+    return {
+      recomendation: [
+        { id: 1, title: "Дитячі товари", image: "img/Medved.png" },
+      ],
+      images: ["@/img/Medved.jpg", "@/img/Medved.svg"],
+    };
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap");
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <Head></Head>
-  <section class = "mt-10 px-6 pb-12">
-    <img src="@/img/Баннер.png" alt="" class = "w-full h-[200px]">
-    <div class="mt-16 grid grid-cols-9 gap-y-3 gap-x-10 md:hidden xl:grid-cols-6 lg:gap-x-2 xl:gap-y-5">
-      <div class = "mx-auto text-center text-base w-[130px]" v-for="rec in recomendation" >
-        <div class = "px-6 py-8 bg-white rounded-full flex items-center justify-center">
-          <img :src="rec.image" alt="" class = "w-[70px] h-[70px]">
+  <section class="mt-10 px-6 pb-12">
+    <img src="@/img/Баннер.png" alt="" class="w-full h-[200px]" />
+    <div
+      class="mt-16 grid grid-cols-9 gap-y-3 gap-x-10 md:hidden xl:grid-cols-6 lg:gap-x-2 xl:gap-y-5"
+    >
+      <div
+        class="mx-auto text-center text-base w-[130px]"
+        v-for="rec in recomendation"
+      >
+        <div
+          class="px-6 py-8 bg-white rounded-full flex items-center justify-center"
+        >
+          <img :src="require('../img/' + rec.image)" alt="" class="h-[70px]" />
         </div>
-        <a href = "#" class = "hover:text-accent">{{ rec.title }}</a>
+        <a href="#" class="hover:text-accent">{{ rec.title }}</a>
       </div>
     </div>
   </section>
@@ -18,42 +25,47 @@
 </template>
 
 <script>
-  import Advan from '@/components/Home_compponents/Advantages.vue'
-  import Head from '@/components/Home_compponents/Head.vue'
-  import Footer from '@/components/Home_compponents/Footer.vue'
-  export default{
-    components:{
-      Head,
-      Footer,
-      Advan
-    },
-    data(){
-      return {
-        recomendation:[
-          {id: 1, title: 'Дитячі товари', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Нерухомість', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Для дому та комерції ', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Робота', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Послуги та фріланс', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Авто', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Запчастини для транспорту', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Тварини', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Електроніка', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Допомога', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Спорт і відпочинок', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Обладнання та сировина', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Будматеріали та інструменти', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Мода та стиль', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Краса і здоров’я', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Обмін', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Для бізнесу', image: '@/img/Баннер.png'},
-          {id: 1, title: 'Хобі та розваги', image: '@/img/Баннер.png'},
-        ],
-        images:[
-          '@/img/Medved.jpg',
-          '@/img/Medved.svg'
-        ]
-      }
-    }
-  }
+import Advan from "@/components/Home_compponents/Advantages.vue";
+import Head from "@/components/Home_compponents/Head.vue";
+import Footer from "@/components/Home_compponents/Footer.vue";
+export default {
+  components: {
+    Head,
+    Footer,
+    Advan,
+  },
+  data() {
+    return {
+      recomendation: [
+        { id: 1, title: "Дитячі товари", image: "Medved.png" },
+        { id: 1, title: "Нерухомість", image: "Баннер.png" },
+        { id: 1, title: "Для дому та комерції ", image: "Баннер.png" },
+        { id: 1, title: "Робота", image: "Баннер.png" },
+        { id: 1, title: "Послуги та фріланс", image: "Баннер.png" },
+        { id: 1, title: "Авто", image: "Баннер.png" },
+        {
+          id: 1,
+          title: "Запчастини для транспорту",
+          image: "Баннер.png",
+        },
+        { id: 1, title: "Тварини", image: "Баннер.png" },
+        { id: 1, title: "Електроніка", image: "Баннер.png" },
+        { id: 1, title: "Допомога", image: "Баннер.png" },
+        { id: 1, title: "Спорт і відпочинок", image: "Баннер.png" },
+        { id: 1, title: "Обладнання та сировина", image: "Баннер.png" },
+        {
+          id: 1,
+          title: "Будматеріали та інструменти",
+          image: "Баннер.png",
+        },
+        { id: 1, title: "Мода та стиль", image: "Баннер.png" },
+        { id: 1, title: "Краса і здоров’я", image: "Баннер.png" },
+        { id: 1, title: "Обмін", image: "Баннер.png" },
+        { id: 1, title: "Для бізнесу", image: "Баннер.png" },
+        { id: 1, title: "Хобі та розваги", image: "Баннер.png" },
+      ],
+      images: ["Medved.jpg", "Medved.svg"],
+    };
+  },
+};
 </script>
