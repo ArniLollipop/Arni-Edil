d777fe3f7d90d736a05aee422ff7dd37ccac6200
Kulzhabay Edil <edil.kulzhabay01@gmail.com>
2022-11-03 23:51:39 +0600

    `RecCards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        main: '#212529',
-        accent: '#8B7AA8',
-        light: '#F5F5F5',
-        dark: '#424242',
-        back: '#E7E7E7',
-        gray: '#999999',
-        pink: '#F2EEF9',
-        blue: '#1437AD',
-        red: '#B73D25',
-        aqua: '#E5F5FF',
-        pink2 : '#E358A6',
+        main: "#212529",
+        accent: "#8B7AA8",
+        light: "#F5F5F5",
+        dark: "#424242",
+        back: "#E7E7E7",
+        gray: "#999999",
+        pink: "#F2EEF9",
+        blue: "#1437AD",
+        red: "#B73D25",
+        aqua: "#E5F5FF",
+        pink2: "#E358A6",
+        green: "#679B00",
       },
-      backgroundImage:{
-        send:'url("@/img/send.svg")',
-        map : 'url("@/img/map.svg")',
-      }
+      backgroundImage: {
+        send: 'url("@/img/send.svg")',
+        map: 'url("@/img/map.svg")',
+      },
     },
   },
   plugins: [],
-}
+};
