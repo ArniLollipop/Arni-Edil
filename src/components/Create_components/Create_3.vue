e04d5fb13d3a9d2618b22b0b89a75f5e32ced992
Kulzhabay Edil <edil.kulzhabay01@gmail.com>
2022-11-11 12:56:16 +0600

    qwe

--- src/components/Create_components/Create_3.vue
+++ src/components/Create_components/Create_3.vue
@@ -349,14 +349,79 @@
 </template>
 
 <script>
-  import Head from '@/components/Head.vue';
-  import Create from '@/components/Create_components/Create.vue';
-  import Create2 from '@/components/Create_components/Create_2.vue';
-  export default{
-    components:{
-      Head,
-      Create,
-      Create2,
-    }
-  }
+import Head from "@/components/Head.vue";
+import Create from "@/components/Create_components/Create.vue";
+import Create2 from "@/components/Create_components/Create_2.vue";
+import { useState } from "@/composables/state";
+export default {
+  setup() {
+    const [dateIndex, setDateIndex] = useState(0);
+    const [arr, setValue] = useState([0, 0, 0, 0, 0, 0, 0]);
+    const [index, setIndex] = useState(0);
+    return {
+      index,
+      setIndex,
+      dateIndex,
+      setDateIndex,
+      arr,
+      setValue,
+    };
+  },
+  components: {
+    Head,
+    Create,
+    Create2,
+  },
+  data() {
+    return {
+      copy: Object.assign([], this.arr),
+      isDatetOpen: false,
+      date: ["7 днів", "1 месяц", "6 месяцев", "год", "1 днів"],
+      harakters: [
+        {
+          id: 0,
+          isOpen: false,
+          title: "Виберіть тип моделі",
+          mas: ["Не обрано", "Текст 1", "Текст 2", "Текст 3", "Текст 4"],
+        },
+        {
+          id: 1,
+          isOpen: false,
+          title: "Приватне-Бізнес",
+          mas: ["Не обрано", "Текст 1", "Текст 2", "Текст 3", "Текст 4"],
+        },
+        {
+          id: 2,
+          isOpen: false,
+          title: "Стан товару",
+          mas: ["Не обрано", "Текст 1", "Текст 2", "Текст 3", "Текст 4"],
+        },
+        {
+          id: 3,
+          isOpen: false,
+          title: "Наявність товару ",
+          mas: ["Не обрано", "Текст 1", "Текст 2", "Текст 3", "Текст 4"],
+        },
+        {
+          id: 4,
+          isOpen: false,
+          title: "Матеріал",
+          mas: ["Не обрано", "Текст 1", "Текст 2", "Текст 3", "Текст 4"],
+        },
+        {
+          id: 5,
+          isOpen: false,
+          title: "Країна виробника",
+          mas: ["Не обрано", "Текст 1", "Текст 2", "Текст 3", "Текст 4"],
+        },
+        {
+          id: 6,
+          isOpen: false,
+          title: "Торг ",
+          mas: ["Не обрано", "Текст 1", "Текст 2", "Текст 3", "Текст 4"],
+        },
+      ],
+    };
+  },
+};
 </script>
