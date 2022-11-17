a5b823bfef2c405edac149544b388f981ed19477
Kulzhabay Edil <edil.kulzhabay01@gmail.com>
2022-11-17 19:10:33 +0600

    supscription

--- src/pages/Subscription.vue
+++ src/pages/Subscription.vue
@@ -113,7 +113,7 @@
                 <div class="text-sm font-medium">{{ item.title }}</div>
                 <div class="text-xs">{{ item.date }}</div>
               </div>
-              <div class="mt-1 ml-3 lg:ml-0 lg:mt-0 space-y-1">
+              <div class="mt-1 ml-3 lg:ml-0 lg:-mt-3 xl:-mt-5 -space-y-3">
                 <div v-for="checkItem in item.check">
                   <input type="checkbox" :id="checkItem.id" />
                   <label :for="checkItem.id" class="text-xs lg:text-sm ml-1">{{
