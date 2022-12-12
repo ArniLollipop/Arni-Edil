67eebbd452b7cb3cfbed1f1fec919abbb5243e48
Arnilollipop <bocunahero@gmail.com>
2022-12-12 19:53:37 +0600

    norm vrode

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,3 +1,4 @@
 <template>
-  <img src="@/img/Баннер.png" alt="" class="w-full 2xl:max-w-[1400px] 2xl:mx-auto max-h-[200px] px-4 pt-4" />
+  <img src="@/img/Баннер.png" alt="" class="w-full 2xl:max-w-[1400px] 2xl:mx-auto max-h-[200px] px-4 pt-4"  />
+  
 </template>
